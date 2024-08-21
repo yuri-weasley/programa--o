eac86ecc6181df6aa8e6ce84f8248dfc91d445e3
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-08-20 21:01:58 -0400

    iniciando com JS

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if(numeroSecreto == chute) {
+    console.log('Isso aí! Você descobriu o número secreto (5)');
+}
