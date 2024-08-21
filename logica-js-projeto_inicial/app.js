170533cab379960ad291dc8c4eb8486390896d80
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-08-21 15:31:46 -0400

    condicionais e concatenação

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,11 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 29;
+let numeroSecreto = 4;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
+// se chute for igual ao número secreto
 if(numeroSecreto == chute) {
-    console.log('Isso aí! Você descobriu o número secreto (5)');
+    alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto})`);
+} else {
+    alert('Você errou :(');
 }
