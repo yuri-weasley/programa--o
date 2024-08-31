d835b244d3dfd5b35586d817139c2e6eb206a07f
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-08-31 19:41:00 -0400

    praticando lógica

---
+++ 3411-praticando-logica-projeto_inicial/ordenador/app.js
@@ -0,0 +1,5 @@
+function ordenarNumeros(a, b, c) {
+    const numerosOrdenados = [a, b, c].sort((x, y) => x - y);
+    console.log(`Números ordenados: ${numerosOrdenados.join(', ')}`);
+}
+ordenarNumeros(3, 1, 7);
--- 3411-praticando-logica-projeto_inicial/palindromos/index.js
+++ 3411-praticando-logica-projeto_inicial/palindromos/index.js
@@ -2,15 +2,24 @@
 
 function essaPalavraEUmPalindromo() {
     var palavra = 'rever';
-    var separandoAsLetrasDaPalavra = palavra.split('');
+    var separandoAsLetrasDaPalavra = palavra.split(''); - "split" função para quebrar a palavra em uma lista de arrays.
     var palavraInvertida = separandoAsLetrasDaPalavra.reverse();
-    palavraInvertida = palavraInvertida.join('');
+    palavraInvertida = palavraInvertida.join(''); - "join" para unir a lista de arrays gerados
     if(palavra == palavraInvertida) {
         console.log("A palavra " + palavra + "é um palíndromo!");
     } else {
         console.log("A palavra " + palavra + "não é um palíndromo!");
     }
-} */
+} 
+    
+function expression: 
+é sempre declarada dentro de uma constante, como abaixo;
+não aceita o comportamento de hoisting;
+
+function declaration:
+é declarada direto, como acima;
+aceita o hoisting;
+*/
 
 const essaPalavraEUmPalindromo = function essaPalavraEUmPalindromo(palavra) {
     var separandoAsLetrasDaPalavra = palavra.split('');
