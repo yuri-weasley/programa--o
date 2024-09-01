4aa6fd771751fd7cfabcb86e4211cdee32ee028e
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-09-01 14:21:32 -0400

    fim do segundo desafio

--- 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -1,21 +1,30 @@
+let totalGeral;
+limpar();
+
 function adicionar() {
     //recuperar valores nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('$')[1];
-    let quantidade = document.getElementById('quantidade');
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-    let preco = quantidade.value * valorUnitario;
-    alert(preco);
-
-    //calcular o preço, o subtotal
+    let quantidade = document.getElementById('quantidade').value;
+   //calcular o preço, o subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
     //adicionar no carrinho
-    //atualizar o valor total
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
     
+    
+    //atualizar o valor total
+    totalGeral += preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar() {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 }
