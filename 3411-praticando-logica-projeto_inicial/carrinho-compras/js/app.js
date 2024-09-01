ae85bcc134b7855fb277e433767f72023adcd00f
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-09-01 15:04:33 -0400

    I implemented the logic of checking a valid entry of input quantity

--- 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ 3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -2,22 +2,28 @@ let totalGeral;
 limpar();
 
 function adicionar() {
-    //recuperar valores nome do produto, quantidade e valor
+    //recuperar valores: "nome do produto", "quantidade" e "valor".
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('$')[1];
     let quantidade = document.getElementById('quantidade').value;
-   //calcular o preço, o subtotal
+
+   //calcular o preço (subtotal).
     let preco = quantidade * valorUnitario;
     let carrinho = document.getElementById('lista-produtos');
-    //adicionar no carrinho
-    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+
+    //adicionar no carrinho.
+    if (quantidade >= 1) {
+        quantidade = quantidade;
+        carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`;
-    
-    
-    //atualizar o valor total
-    totalGeral += preco;
+        totalGeral += preco;
+    } else if(quantidade < 1) {
+        alert('Digite uma quantidade válida.');
+    }
+        
+    //atualizar o valor total.
     let campoTotal = document.getElementById('valor-total');
     campoTotal.textContent = `R$ ${totalGeral}`;
     document.getElementById('quantidade').value = 0;
