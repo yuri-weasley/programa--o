84f328b7346727dc76bcdbd32ad279c487d9fabf
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-09-07 14:56:14 -0400

    pratica com o artigo da Jacque

--- javaTeste/artigo-da-jacque/src/artigo.java
+++ javaTeste/artigo-da-jacque/src/artigo.java
@@ -5,4 +5,46 @@ public class artigo {
     private Autor autor;
     private LocalDate dataPublicacao;
     private Categoria categoria;
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public Autor getAutor() {
+        return autor;
+    }
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+    public Categoria getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    //constructor...
+
+    public artigo(String titulo, Autor autor, LocalDate dataPublicacao, Categoria categoria) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataPublicacao = dataPublicacao;
+        this.categoria = categoria;
+    }
+    
+    @Override
+    public String toString() {
+        return "artigo [titulo=" + titulo + ", autor=" + autor + ", dataPublicacao=" + dataPublicacao + ", categoria="
+                + categoria + "]";
+    }
+
+    
 }
