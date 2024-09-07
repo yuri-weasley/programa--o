15f349675e9b5fab011805e9b738ba96dab92c79
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-09-07 14:40:47 -0400

    primeiro contato com o artigo da Jacque

---
+++ javaTeste/artigo-da-jacque/src/artigo.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class artigo {
+    private String titulo;
+    private Autor autor;
+    private LocalDate dataPublicacao;
+    private Categoria categoria;
+}
