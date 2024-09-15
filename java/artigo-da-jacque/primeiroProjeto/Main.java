88983f8d012b485dc492bd8101ee50411f150f7e
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-09-15 06:58:16 -0400

    new java project

---
+++ java/artigo-da-jacque/primeiroProjeto/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: "+ anoDeLancamento);
+    }
+}
