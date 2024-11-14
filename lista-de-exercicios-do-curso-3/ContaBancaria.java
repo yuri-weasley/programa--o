2808cd2f24e5659a53d769d15e07adfef85a54b0
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-11-13 20:01:15 -0400

    finalizando mais desafios

--- lista-de-exercicios-do-curso-3/ContaBancaria.java
+++ lista-de-exercicios-do-curso-3/ContaBancaria.java
@@ -1,17 +1,16 @@
 public class ContaBancaria {
     private int numeroConta;
     private double saldo;
+
+    public ContaBancaria(int numeroConta, double saldo) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
     
     public int getNumeroConta() {
         return numeroConta;
     }
-    public void setNumeroConta(int numeroConta) {
-        this.numeroConta = numeroConta;
-    }
     public double getSaldo() {
         return saldo;
-    }
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
     }
 }
