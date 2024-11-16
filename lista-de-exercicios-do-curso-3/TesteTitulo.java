bf391c195b6b8befcc692763a6798b506d0b57f6
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-11-16 13:15:27 -0400

    resolução de mais atividades

--- lista-de-exercicios-do-curso-3/TesteTitulo.java
+++ lista-de-exercicios-do-curso-3/TesteTitulo.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class TesteTitulo {
     public static void main(String[] args) {
@@ -13,5 +15,17 @@ public class TesteTitulo {
         for (Titulo titulo : listaDeTitus) {
             System.out.println(titulo.nome);
         }
+
+        List<String> listaPolimorfica;
+
+        listaPolimorfica = new ArrayList<>();
+        listaPolimorfica.add("Elemento 1");
+        listaPolimorfica.add("Elemento 2");
+        System.out.println("ArrayList: " + listaPolimorfica);
+
+        listaPolimorfica = new LinkedList<>();
+        listaPolimorfica.add("Elemento A");
+        listaPolimorfica.add("Elemento B");
+        System.out.println("LinkedList: " + listaPolimorfica);
     }
 }
