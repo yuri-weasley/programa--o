fe7150e6180a3a269be29d7e49a7324467964604
yuri-weasley <124360833+yuri-weasley@users.noreply.github.com>
2024-11-27 20:40:08 -0400

    inserindo a request

--- java/screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ java/screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,16 +5,22 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        String busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=cbfee9f5";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-         .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=cbfee9f5"))
-         .build();
-         HttpResponse<String> response = client
-     .send(request, BodyHandlers.ofString()); 
-     System.out.println(response.body());
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
